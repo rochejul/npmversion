e2f08c83a90c2ba4f5c51c35f8b288c9bbcad556
Julien Roche <roche.jul@gmail.com>
2025-01-19 18:06:25 +0100

    feat(workspace): base of logging

--- packages/workspace/src/npm/index.js
+++ packages/workspace/src/npm/index.js
@@ -1,6 +1,7 @@
 import { computeWorkspace } from '../workspace.js';
 import { updateRootVersion } from './without-workspaces.js';
 import { updateWorkspaceVersion } from './with-workspaces.js';
+import ProcLog from 'proc-log';
 
 /**
  * Update the package.json file, package=lock.json file and if needed workspaces
@@ -9,11 +10,18 @@ import { updateWorkspaceVersion } from './with-workspaces.js';
  * @returns {Promise<string}
  */
 export async function updatePackageVersion(packageVersion, cwd) {
+  ProcLog.log.info('@npmcli/workspace - updatePackageVersion');
   const workspace = await computeWorkspace(cwd);
 
   if (workspace.isLeaf()) {
+    ProcLog.log.info(
+      '@npmcli/workspace - updatePackageVersion - no workspace detected',
+    );
     await updateRootVersion(packageVersion, cwd);
   } else {
+    ProcLog.log.info(
+      '@npmcli/workspace - updatePackageVersion - workspace detected',
+    );
     await updateWorkspaceVersion(workspace, packageVersion, cwd);
   }
 
